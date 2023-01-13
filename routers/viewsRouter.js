303c9074aae9af3a563aa032efac5af1da91f2de
tel3port <carn3lian@gmail.com>
2023-01-13 03:14:00 +0300

    added css for auth page

--- routers/viewsRouter.js
+++ routers/viewsRouter.js
@@ -5,9 +5,9 @@ const viewsController = require("../controllers/viewsController.js")
 const viewsRouter = express.Router()
 
 viewsRouter.route("/").get(viewsController.getHomePage)
-viewsRouter.route("/:moviename").get(viewsController.getDetailPage)
-viewsRouter.route("/dashboard").get(viewsController.getDashboard)
 viewsRouter.route("/auth").get(viewsController.getAuthWall)
+viewsRouter.route("/watch/:moviename").get(viewsController.getDetailPage)
+viewsRouter.route("/dashboard").get(viewsController.getDashboard)
 
 
 
